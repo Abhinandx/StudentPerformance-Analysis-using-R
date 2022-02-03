4f39809ff78c9a1692312a4f3822cde43e4085d0
Abhinand Hareendran <78462961+Abhinandx@users.noreply.github.com>
2022-02-03 23:02:39 +0530

    added the code and csv file
    The data set StudentsPerformance.csv was added along with the code

---
+++ studentperfomancecode.r
@@ -0,0 +1,38 @@
+print(getwd())
+
+setwd("D:/Abhinand Hareendran/3 year/Sem 5/Data mining")
+# Importing the dataset
+dataset = read.csv('StudentsPerformance.csv')
+
+# Adding average column in the dataset
+dataset$average<-(dataset[,6]+dataset[,7]+dataset[,8])/3
+print(dataset)
+View(dataset)
+
+library(rpart)
+library(rpart.plot)
+
+s<-sample(1000,100)
+s
+dataset_train <- dataset[s,]
+dataset_test <- dataset[-s,]
+
+tree<-rpart(race.ethnicity~average, dataset_train)
+tree
+plot(tree)
+text(tree)
+rpart.plot(tree)
+
+tree<-rpart(gender~average, dataset_train,method="class")
+tree
+plot(tree)
+text(tree)
+rpart.plot(tree)
+
+tree<-rpart(gender~math.score+reading.score+writing.score, dataset_train, method="class")
+tree
+plot(tree)
+text(tree)
+rpart.plot(tree)
+
+
